90e25c7552d809cdadcef48b0cb24543f7ce8c71
Wojbor <39215797+Wojbor@users.noreply.github.com>
2021-10-04 12:10:38 +0200

    Added Specialties Map Service impl. closes #31

---
+++ pet-clinic-data/src/main/java/amaciag/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package amaciag.springframework.sfgpetclinic.services;
+
+import amaciag.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
